fc52d222f1230ed4665f9f96c69cebbbbad58184
whitegoldukhta <james.99@list.ru>
2022-01-21 22:27:52 +0200

    Spring project

--- Spring_HelloWorld/src/main/java/App.java
+++ Spring_HelloWorld/src/main/java/App.java
@@ -8,5 +8,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        PojoCat beanCat = (PojoCat) applicationContext.getBean("cat");
+        PojoCat beanCat1 = (PojoCat) applicationContext.getBean("cat");
+        boolean comparisonCats = beanCat1.getName().equals(beanCat.getName());
+        boolean comparisonHello = (bean.getMessage()).equals(bean1.getMessage());
+        System.out.println(comparisonCats);
+        System.out.println(comparisonHello);
     }
 }
---
+++ Spring_HelloWorld/src/main/java/PojoCat.java
@@ -0,0 +1,11 @@
+public class PojoCat {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
